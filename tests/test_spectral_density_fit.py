e1d41f16d80ac97ff1045efd160c4453204ae240
Johannes Feist <johannes.feist@gmail.com>
2025-03-26 11:50:40 +0100

    some extra tests

--- tests/test_spectral_density_fit.py
+++ tests/test_spectral_density_fit.py
@@ -23,36 +23,53 @@ def get_test_data(complex):
 
     return Nm, Ne, ω, Heff, g
 
-def test_Jmod_real():
-    Nm, Ne, ω, Heff, g = get_test_data(False)
+def _test_Jmod(complex):
+    Nm, Ne, ω, Heff, g = get_test_data(complex)
     J1 = Jmod(ω,Heff,g)
     J2 = Jmod_naive(ω,Heff,g)
-    assert np.isrealobj(J1)
-    assert np.isrealobj(J2)
+    istypeobj = np.iscomplexobj if complex else np.isrealobj
+    assert istypeobj(J1)
+    assert istypeobj(J2)
     assert np.allclose(J1,J2)
+
+def test_Jmod_real():
+    _test_Jmod(False)
 
 def test_Jmod_complex():
-    Nm, Ne, ω, Heff, g = get_test_data(True)
-    J1 = Jmod(ω,Heff,g)
-    J2 = Jmod_naive(ω,Heff,g)
-    assert np.iscomplexobj(J1)
-    assert np.iscomplexobj(J2)
-    assert np.allclose(J1,J2)
+    _test_Jmod(True)
 
-def test_real():
-    Nm, Ne, ω, Heff, g = get_test_data(False)
+def _test_gradient(complex):
+    Nm, Ne, ω, Heff, g = get_test_data(complex)
+    J = Jmod(ω,Heff,g)
+    opt_1 = spectral_density_fitter(ω,J,Nm,diagonalize=False)
+    opt_2 = spectral_density_fitter(ω,J,Nm,diagonalize=True)
+
+    ps = opt_1.Hκg_to_ps(Heff.real + 0.03*((x:=np.random.rand(Nm,Nm)) + x.T), -2*Heff.imag.diagonal() + 0.1*np.random.rand(Nm), g + 0.1*np.random.rand(Ne,Nm))
+    grad_1 = np.empty_like(ps,dtype=np.complex128)
+    grad_2 = np.empty_like(grad_1)
+    err_1 = opt_1.obj_fun(ps,grad_1)
+    err_2 = opt_2.obj_fun(ps,grad_2)
+    assert np.allclose(err_1,err_2)
+    assert np.allclose(grad_1,grad_2)
+
+def test_gradient():
+    for complex in (False,True):
+        _test_gradient(complex)
+
+def _test_fitting(complex,thresh):
+    Nm, Ne, ω, Heff, g = get_test_data(complex)
     J = Jmod(ω,Heff,g)
     opt = spectral_density_fitter(ω,J,Nm)
     ps = opt.Hκg_to_ps(Heff.real + 0.03*((x:=np.random.rand(Nm,Nm)) + x.T), -2*Heff.imag.diagonal() + 0.1*np.random.rand(Nm), g + 0.1*np.random.rand(Ne,Nm))
     ps = opt.optimize(ps)
-    assert np.isrealobj(opt.Jfun(ω,ps))
-    assert opt.obj_fun(ps,np.empty(0)) < 0.001
+    if complex:
+        assert np.iscomplexobj(opt.Jfun(ω,ps))
+    else:
+        assert np.isrealobj(opt.Jfun(ω,ps))
+    assert opt.obj_fun(ps,np.empty(0)) < thresh
 
-def test_complex():
-    Nm, Ne, ω, Heff, g = get_test_data(True)
-    J = Jmod(ω,Heff,g)
-    opt = spectral_density_fitter(ω,J,Nm)
-    ps = opt.Hκg_to_ps(Heff.real + 0.03*((x:=np.random.rand(Nm,Nm)) + x.T), -2*Heff.imag.diagonal() + 0.1*np.random.rand(Nm), g + 0.1*np.random.rand(Ne,Nm))
-    ps = opt.optimize(ps)
-    assert np.iscomplexobj(opt.Jfun(ω,ps))
-    assert opt.obj_fun(ps,np.empty(0)) < 0.005
+def test_fitting_real():
+    _test_fitting(False,0.001)
+
+def test_fitting_complex():
+    _test_fitting(True,0.005)
