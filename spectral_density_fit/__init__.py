45a58cd137f71f62be9e2d4ed1742a0c1d2a4e76
Johannes Feist <johannes.feist@gmail.com>
2025-03-26 12:00:09 +0100

    add ruff formatting settings to pyproject.toml, and apply it to the codes

--- spectral_density_fit/__init__.py
+++ spectral_density_fit/__init__.py
@@ -5,7 +5,7 @@ Fernández-Domínguez, and J. Feist, Phys. Rev. Lett. 126, 093601 (2021),
 https://doi.org/10.1103/PhysRevLett.126.093601
 """
 
-__version__ = '0.2.0'
+__version__ = "0.2.0"
 __all__ = ["spectral_density_fitter"]
 
 import jax
@@ -16,10 +16,12 @@ from functools import partial
 import numpy as np
 import nlopt
 
+
 @jit
-def Jmod_lorentz(ω,λs,g):
-    χ = jnp.einsum('il,jl,lw->ijw',g,g,1/(λs[:,None]-ω[None,:]))
-    return χ.imag/jnp.pi
+def Jmod_lorentz(ω, λs, g):
+    χ = jnp.einsum("il,jl,lw->ijw", g, g, 1 / (λs[:, None] - ω[None, :]))
+    return χ.imag / jnp.pi
+
 
 """this solves the linear equation at each frequency. it is less
 efficient than diagonalizing Heff, but jax can calculate the gradient directly,
@@ -32,69 +34,76 @@ Note that the diagonalization-based routines assume that H is complex symmetric,
 H = H^T (and thus H^\\dagger = H^*), while this one does not, so it is in
 principle more general.
 """
+
+
 @jit
-def Jmod_naive(ω,Heff,g):
+def Jmod_naive(ω, Heff, g):
     I = jnp.eye(Heff.shape[0])
-    Hω = Heff[None,:,:] - ω[:,None,None]*I[None,:,:]
+    Hω = Heff[None, :, :] - ω[:, None, None] * I[None, :, :]
     # χ = 1/(H-w)
     # χ = χ^T -> χ^* = χ^\dagger
     # Im(χ) = (χ-χ^*) / 2i = (χ-χ^\dagger) / 2i
     # J = g @ Im(χ) @ g^\dagger / π = g @ (χ-χ^\dagger) @ g^\dagger / 2iπ
     # R = g @ χ @ g^\dagger
     # J = (R - R^\dagger) / 2iπ
-    R = g @ jnp.linalg.solve(Hω, g.conj().T[None,:,:])
+    R = g @ jnp.linalg.solve(Hω, g.conj().T[None, :, :])
     if jnp.iscomplexobj(g) and g.shape[0] > 1:
         # if g is complex and Ne>1, J is a Hermitian matrix with complex off-diagonal elements.
         # indices of R are (w,i,j), R^\dagger = R.conj()[w,j,i], transpose to (i,j,w)
-        return ((R.transpose(1,2,0) - R.conj().transpose(2,1,0)) / (2j*jnp.pi))
+        return (R.transpose(1, 2, 0) - R.conj().transpose(2, 1, 0)) / (2j * jnp.pi)
     else:
         # we assume that H is complex symmetric, so for real g, R = R^T
         # -> J = R.imag / π (and is thus explicitly real)
-        return R.imag.transpose(1,2,0) / jnp.pi
+        return R.imag.transpose(1, 2, 0) / jnp.pi
+
 
 # fχ gives a jax tracer leak error if jitted in a function called with ω as an
 # explicit argument in newer jax versions (certainly in 0.4.14, not sure since
 # when, 0.2.19 was fine) due to the custom_jvp definition. So define the function
 # without jit, and below provide the jitted wrapper
-def _non_jitted_Jmod(ω,Heff,g):
+def _non_jitted_Jmod(ω, Heff, g):
     # χ = 1/(H-w)
-    χ = fχ(ω,Heff)
+    χ = fχ(ω, Heff)
     # J = g Im(χ) g^\dagger / π
     # the .astype(g.dtype) ensures that no warnings are raised
-    return jnp.einsum('il,lmw,jm->ijw', g, χ.imag.astype(g.dtype), g.conj()) / jnp.pi
+    return jnp.einsum("il,lmw,jm->ijw", g, χ.imag.astype(g.dtype), g.conj()) / jnp.pi
 
-def Jmod(ω,Heff,g):
+
+def Jmod(ω, Heff, g):
     # diagonalization is not supported on GPUs, so we need to run this on CPU
     with jax.default_device(jax.devices("cpu")[0]):
-        return jit(_non_jitted_Jmod)(ω,Heff,g)
+        return jit(_non_jitted_Jmod)(ω, Heff, g)
+
 
 @partial(jax.custom_jvp, nondiff_argnums=(0,))
-def fχ(ω,Heff):
+def fχ(ω, Heff):
     λs, V = jnp.linalg.eig(Heff)
-    V /= jnp.sqrt(jnp.sum(V**2,axis=0))
-    return jnp.einsum('il,lw,jl->ijw',V,1/(λs[:,None]-ω[None,:]),V)
+    V /= jnp.sqrt(jnp.sum(V**2, axis=0))
+    return jnp.einsum("il,lw,jl->ijw", V, 1 / (λs[:, None] - ω[None, :]), V)
+
 
 @fχ.defjvp
-def fχ_jvp(ω,primals,tangents):
+def fχ_jvp(ω, primals, tangents):
     # χ = 1/(H-w)
     # express dχ in terms of χ, H, dH by using that it solves the linear equation
     # (H-w) χ = 1
     # ∂H χ + (H-w) ∂χ = 0
     # ∂χ = 1/(H-w) (-∂H χ) = -χ ∂H χ
-    Heff, = primals
-    dHeff, = tangents
+    (Heff,) = primals
+    (dHeff,) = tangents
 
     λs, V = jnp.linalg.eig(Heff)
-    V /= jnp.sqrt(jnp.sum(V**2,axis=0))
-    λωinv = 1/(λs[:,None]-ω[None,:])
+    V /= jnp.sqrt(jnp.sum(V**2, axis=0))
+    λωinv = 1 / (λs[:, None] - ω[None, :])
 
-    χ = jnp.einsum('il,lw,jl->ijw',V,λωinv,V)
-    dχ = jnp.einsum('imw,ml,ljw->ijw',χ,-dHeff,χ)
+    χ = jnp.einsum("il,lw,jl->ijw", V, λωinv, V)
+    dχ = jnp.einsum("imw,ml,ljw->ijw", χ, -dHeff, χ)
     return χ, dχ
+
 
 # version that allows to pass "templates" for H and g that
 # indicate where they are allowed to be nonzero in the fit
-def spectral_density_fitter(ω,J,Hgtmpl,λlims=None,fitlog=False,diagonalize=None,device=None):
+def spectral_density_fitter(ω, J, Hgtmpl, λlims=None, fitlog=False, diagonalize=None, device=None):
     if diagonalize is None:
         # if diagonalize is not set explicitly, default to it while running on CPU
         # but use direct inversion on GPU (since non-Hermitian diagonalization is
@@ -109,21 +118,21 @@ def spectral_density_fitter(ω,J,Hgtmpl,λlims=None,fitlog=False,diagonalize=Non
         ω = jnp.array(ω)
         J = jnp.array(J)
         if J.ndim == 1:
-            J = J[None,None,:]
+            J = J[None, None, :]
 
-        if isinstance(Hgtmpl,tuple):
+        if isinstance(Hgtmpl, tuple):
             Htmpl, gtmpl = Hgtmpl
             Ne, Nm = gtmpl.shape
-            assert Htmpl.shape == (Nm,Nm)
+            assert Htmpl.shape == (Nm, Nm)
         else:
             Nm = int(Hgtmpl)
             Ne = J.shape[0]
-            Htmpl = jnp.ones((Nm,Nm))
-            gtmpl = jnp.ones((Ne,Nm))
+            Htmpl = jnp.ones((Nm, Nm))
+            gtmpl = jnp.ones((Ne, Nm))
 
-        assert J.shape == (Ne,Ne,len(ω))
+        assert J.shape == (Ne, Ne, len(ω))
 
-        if fitlog and Ne>1:
+        if fitlog and Ne > 1:
             raise ValueError(f"fitlog=True only supported for 1 emitter. Got Ne = {Ne}.")
 
         # get the indices of the nonzero entries in the upper triangle of Htmpl
@@ -133,100 +142,102 @@ def spectral_density_fitter(ω,J,Hgtmpl,λlims=None,fitlog=False,diagonalize=Non
         Nps_H = len(H_inds[0])
         Nps_g = len(g_inds[0])
 
-        tmpH = jnp.zeros((Nm,Nm))
-        tmpg = jnp.zeros((Ne,Nm))
+        tmpH = jnp.zeros((Nm, Nm))
+        tmpg = jnp.zeros((Ne, Nm))
 
         if jnp.iscomplexobj(J):
-            Nps = 2*Nps_g + Nm + Nps_H
+            Nps = 2 * Nps_g + Nm + Nps_H
 
-            def Hκg_to_ps(H,κ,g):
+            def Hκg_to_ps(H, κ, g):
                 with jax.default_device(device):
                     ps = jnp.hstack((g[g_inds].real, g[g_inds].imag, jnp.sqrt(κ), H[H_inds]))
                 # commit to the device
-                return jax.device_put(ps,device)
+                return jax.device_put(ps, device)
 
             @jit
             def ps_to_Hκg(ps):
-                gps, gpsc, sqrtκ,Hps = jnp.split(ps,[Nps_g, 2*Nps_g, 2*Nps_g+Nm])
-                κ  = sqrtκ**2
-                g  = tmpg.at[g_inds].set(gps) + 1j * tmpg.at[g_inds].set(gpsc)
+                gps, gpsc, sqrtκ, Hps = jnp.split(ps, [Nps_g, 2 * Nps_g, 2 * Nps_g + Nm])
+                κ = sqrtκ**2
+                g = tmpg.at[g_inds].set(gps) + 1j * tmpg.at[g_inds].set(gpsc)
                 Hu = tmpH.at[H_inds].set(Hps)
-                H = Hu + jnp.tril(Hu.T,-1)
-                return H,κ,g
+                H = Hu + jnp.tril(Hu.T, -1)
+                return H, κ, g
         else:
             Nps = Nps_g + Nm + Nps_H
 
-            def Hκg_to_ps(H,κ,g):
+            def Hκg_to_ps(H, κ, g):
                 with jax.default_device(device):
-                    ps = jnp.hstack((g[g_inds],jnp.sqrt(κ),H[H_inds]))
+                    ps = jnp.hstack((g[g_inds], jnp.sqrt(κ), H[H_inds]))
                 # commit to the device
-                return jax.device_put(ps,device)
+                return jax.device_put(ps, device)
 
             @jit
             def ps_to_Hκg(ps):
-                gps,sqrtκ,Hps = jnp.split(ps,[Nps_g,Nps_g+Nm])
-                κ  = sqrtκ**2
-                g  = tmpg.at[g_inds].set(gps)
+                gps, sqrtκ, Hps = jnp.split(ps, [Nps_g, Nps_g + Nm])
+                κ = sqrtκ**2
+                g = tmpg.at[g_inds].set(gps)
                 Hu = tmpH.at[H_inds].set(Hps)
-                H = Hu + jnp.tril(Hu.T,-1)
-                return H,κ,g
+                H = Hu + jnp.tril(Hu.T, -1)
+                return H, κ, g
 
         if diagonalize:
-            def Jfun(ω,ps):
-                H,κ,g = ps_to_Hκg(ps)
-                Heff = H-0.5j*jnp.diag(κ)
-                JJ = _non_jitted_Jmod(ω,Heff,g)
+
+            def Jfun(ω, ps):
+                H, κ, g = ps_to_Hκg(ps)
+                Heff = H - 0.5j * jnp.diag(κ)
+                JJ = _non_jitted_Jmod(ω, Heff, g)
                 return JJ
         else:
-            def Jfun(ω,ps):
-                H,κ,g = ps_to_Hκg(ps)
-                Heff = H-0.5j*jnp.diag(κ)
-                JJ = Jmod_naive(ω,Heff,g)
+
+            def Jfun(ω, ps):
+                H, κ, g = ps_to_Hκg(ps)
+                Heff = H - 0.5j * jnp.diag(κ)
+                JJ = Jmod_naive(ω, Heff, g)
                 return JJ
 
         @jit
         def err(ps):
-            Jf = Jfun(ω,ps)
+            Jf = Jfun(ω, ps)
             assert jnp.iscomplexobj(Jf) == jnp.iscomplexobj(J)
             if fitlog:
                 return jnp.linalg.norm(jnp.log(Jf) - jnp.log(J))
             else:
-                return jnp.linalg.norm(Jf-J)
+                return jnp.linalg.norm(Jf - J)
 
         grad_err = jit(grad(err))
 
-        def nlopt_f(ps,grad):
-            if grad.size>0:
+        def nlopt_f(ps, grad):
+            if grad.size > 0:
                 grad[...] = grad_err(ps)
             return float(err(ps))
 
         @jit
         def f_constraints(ps):
             "constraint function that forces eigenvalues to be within the range [λmin,λmax]"
-            H,κ,g = ps_to_Hκg(ps)
+            H, κ, g = ps_to_Hκg(ps)
             λs = jnp.linalg.eigvalsh(H)
             # nlopt enforces constraint functions to be smaller than 0
-            return jnp.hstack((λs-λmax,λmin-λs))
+            return jnp.hstack((λs - λmax, λmin - λs))
 
         jac_constraints = jit(jacobian(f_constraints))
 
-        def nlopt_constraints(result,ps,grad):
+        def nlopt_constraints(result, ps, grad):
             result[...] = f_constraints(ps)
             if grad.size > 0:
                 grad[...] = jac_constraints(ps)
 
-        opt = nlopt.opt(nlopt.LD_MMA,Nps)
+        opt = nlopt.opt(nlopt.LD_MMA, Nps)
         opt.set_min_objective(nlopt_f)
         opt.set_ftol_rel(1e-5)
 
         if λlims is not False:
-            λmin, λmax = (ω[0],ω[-1]) if λlims is None else λlims
-            opt.add_inequality_mconstraint(nlopt_constraints, np.zeros(2*Nm))
+            λmin, λmax = (ω[0], ω[-1]) if λlims is None else λlims
+            opt.add_inequality_mconstraint(nlopt_constraints, np.zeros(2 * Nm))
 
         # add members to opt to have everything in a single object
         opt.Hκg_to_ps = Hκg_to_ps
         opt.ps_to_Hκg = ps_to_Hκg
-        opt.Jfun = jit(Jfun) # we can jit this because it is not used in the optimization
+        opt.Jfun = jit(Jfun)  # we can jit this because it is not used in the optimization
         opt.obj_fun = nlopt_f
 
     return opt
--- tests/test_spectral_density_fit.py
+++ tests/test_spectral_density_fit.py
@@ -2,74 +2,85 @@ from spectral_density_fit import __version__, spectral_density_fitter, Jmod, Jmo
 
 # run the tests in 64-bit precision
 import jax
+
 jax.config.update("jax_enable_x64", True)
 
 import numpy as np
 
+
 def test_version():
-    assert __version__ == '0.2.0'
+    assert __version__ == "0.2.0"
+
 
 def get_test_data(complex):
     Nm = 3
     Ne = 4
     np.random.seed(1234)
-    ω = np.linspace(-3.5,3.5,101)
-    H = np.random.normal(size=(Nm,Nm))
-    Heff = H + H.T - 0.5j*np.diag(np.random.rand(Nm))
+    ω = np.linspace(-3.5, 3.5, 101)
+    H = np.random.normal(size=(Nm, Nm))
+    Heff = H + H.T - 0.5j * np.diag(np.random.rand(Nm))
 
-    g = np.random.normal(size=(Ne,Nm))
+    g = np.random.normal(size=(Ne, Nm))
     if complex:
-        g = g + 1j*np.random.normal(size=(Ne,Nm))
+        g = g + 1j * np.random.normal(size=(Ne, Nm))
 
     return Nm, Ne, ω, Heff, g
 
+
 def _test_Jmod(complex):
     Nm, Ne, ω, Heff, g = get_test_data(complex)
-    J1 = Jmod(ω,Heff,g)
-    J2 = Jmod_naive(ω,Heff,g)
+    J1 = Jmod(ω, Heff, g)
+    J2 = Jmod_naive(ω, Heff, g)
     istypeobj = np.iscomplexobj if complex else np.isrealobj
     assert istypeobj(J1)
     assert istypeobj(J2)
-    assert np.allclose(J1,J2)
+    assert np.allclose(J1, J2)
+
 
 def test_Jmod_real():
     _test_Jmod(False)
 
+
 def test_Jmod_complex():
     _test_Jmod(True)
 
+
 def _test_gradient(complex):
     Nm, Ne, ω, Heff, g = get_test_data(complex)
-    J = Jmod(ω,Heff,g)
-    opt_1 = spectral_density_fitter(ω,J,Nm,diagonalize=False)
-    opt_2 = spectral_density_fitter(ω,J,Nm,diagonalize=True)
+    J = Jmod(ω, Heff, g)
+    opt_1 = spectral_density_fitter(ω, J, Nm, diagonalize=False)
+    opt_2 = spectral_density_fitter(ω, J, Nm, diagonalize=True)
 
-    ps = opt_1.Hκg_to_ps(Heff.real + 0.03*((x:=np.random.rand(Nm,Nm)) + x.T), -2*Heff.imag.diagonal() + 0.1*np.random.rand(Nm), g + 0.1*np.random.rand(Ne,Nm))
-    grad_1 = np.empty_like(ps,dtype=np.complex128)
+    ps = opt_1.Hκg_to_ps(Heff.real + 0.03 * ((x := np.random.rand(Nm, Nm)) + x.T), -2 * Heff.imag.diagonal() + 0.1 * np.random.rand(Nm), g + 0.1 * np.random.rand(Ne, Nm))
+    grad_1 = np.empty_like(ps, dtype=np.complex128)
     grad_2 = np.empty_like(grad_1)
-    err_1 = opt_1.obj_fun(ps,grad_1)
-    err_2 = opt_2.obj_fun(ps,grad_2)
-    assert np.allclose(err_1,err_2)
-    assert np.allclose(grad_1,grad_2)
+    err_1 = opt_1.obj_fun(ps, grad_1)
+    err_2 = opt_2.obj_fun(ps, grad_2)
+    assert np.allclose(err_1, err_2)
+    assert np.allclose(grad_1, grad_2)
+
 
 def test_gradient():
-    for complex in (False,True):
+    for complex in (False, True):
         _test_gradient(complex)
 
-def _test_fitting(complex,thresh):
+
+def _test_fitting(complex, thresh):
     Nm, Ne, ω, Heff, g = get_test_data(complex)
-    J = Jmod(ω,Heff,g)
-    opt = spectral_density_fitter(ω,J,Nm)
-    ps = opt.Hκg_to_ps(Heff.real + 0.03*((x:=np.random.rand(Nm,Nm)) + x.T), -2*Heff.imag.diagonal() + 0.1*np.random.rand(Nm), g + 0.1*np.random.rand(Ne,Nm))
+    J = Jmod(ω, Heff, g)
+    opt = spectral_density_fitter(ω, J, Nm)
+    ps = opt.Hκg_to_ps(Heff.real + 0.03 * ((x := np.random.rand(Nm, Nm)) + x.T), -2 * Heff.imag.diagonal() + 0.1 * np.random.rand(Nm), g + 0.1 * np.random.rand(Ne, Nm))
     ps = opt.optimize(ps)
     if complex:
-        assert np.iscomplexobj(opt.Jfun(ω,ps))
+        assert np.iscomplexobj(opt.Jfun(ω, ps))
     else:
-        assert np.isrealobj(opt.Jfun(ω,ps))
-    assert opt.obj_fun(ps,np.empty(0)) < thresh
+        assert np.isrealobj(opt.Jfun(ω, ps))
+    assert opt.obj_fun(ps, np.empty(0)) < thresh
+
 
 def test_fitting_real():
-    _test_fitting(False,0.001)
+    _test_fitting(False, 0.001)
+
 
 def test_fitting_complex():
-    _test_fitting(True,0.005)
+    _test_fitting(True, 0.005)
